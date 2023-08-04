d33cbe4c589565d8426484ec96963ce8fc0da070
rameezqazi23 <rameez.qazi23@gmail.com>
2023-08-04 19:54:06 +0500

    setting up firebase environment for user authnetication only email and password for now

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyABIpfqKVdaBPyLhT4lx3OvOQYVCkkvlfs",
+    authDomain: "crypto-api-531bf.firebaseapp.com",
+    projectId: "crypto-api-531bf",
+    storageBucket: "crypto-api-531bf.appspot.com",
+    messagingSenderId: "335401689675",
+    appId: "1:335401689675:web:c405c1ee94d95487ec63ce"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app
