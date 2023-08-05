f39f335224c3cef4ed74c9a192cd14dbee435bcc
rameezqazi23 <rameez.qazi23@gmail.com>
2023-08-05 19:54:01 +0500

    setting up firestore add data to the firestore but not getting data through onSnapshot? will try to fix them

--- src/Components/CoinItem.jsx
+++ src/Components/CoinItem.jsx
@@ -1,49 +1,82 @@
-import React from 'react';
-import { AiOutlineStar } from 'react-icons/ai';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { Sparklines, SparklinesLine } from "react-sparklines";
+import { Link } from "react-router-dom";
+import { UserAuth } from "../Context/AuthContext";
+import { addDoc, arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { db } from "../Firebase/Firebase";
 
 const CoinItem = ({ coin }) => {
+  const [favCoin, setFavCoin] = useState(false);
 
-    const x = coin.price_change_percentage_24h
-    console.log(x.toFixed(1))
+  const { user } = UserAuth();
 
-    return (
+  const x = coin.price_change_percentage_24h;
+  console.log(x.toFixed(1));
+  console.log("Coin Saved", favCoin);
 
-        <tr className='h-[80px] border-b overflow-hidden'>
-            <td><AiOutlineStar /></td>
-            <td>{coin.market_cap_rank}</td>
-            <td>
-                <Link to={`/coin/${coin.id}`}>
-                    <div className='flex items-center'>
-                        <img className='w-6 mr-2 rounded-full' src={coin.image} alt={coin.id} />
-                        <p className='hidden sm:table-cell'>{coin.name}</p>
-                    </div>
-                </Link>
-            </td>
-            <td>{coin.symbol.toUpperCase()}</td>
-            <td>${coin.current_price.toLocaleString()}</td>
-            <td>
+  const coinPath = doc(db, "users", `${user?.email}`);
+  const addCoin = async () => {
+    if (user?.email) {
+      setFavCoin(true);
+      await updateDoc(coinPath, {
+        bookmarks: arrayUnion({
+          id: coin.id,
+          image: coin.image,
+          name: coin.name,
+          symbol: coin.symbol,
+          rank: coin.market_cap_rank,
+        }),
+      });
+    } else {
+      alert("Sign in first to save favourite coins");
+    }
+  };
 
-                {coin.price_change_percentage_24h > 0 ?
-                    (<p className='text-green-600'>{coin.price_change_percentage_24h.toFixed(2)}%</p>)
-                    : (<p className='text-red-600'>{coin.price_change_percentage_24h.toFixed(2)}%</p>)}
+  return (
+    <tr className="h-[80px] border-b overflow-hidden">
+      <td onClick={addCoin}>
+        {favCoin ? <AiFillStar /> : <AiOutlineStar />}
+    </td>
+      <td>{coin.market_cap_rank}</td>
+      <td>
+        <Link to={`/coin/${coin.id}`}>
+          <div className="flex items-center">
+            <img
+              className="w-6 mr-2 rounded-full"
+              src={coin.image}
+              alt={coin.id}
+            />
+            <p className="hidden sm:table-cell">{coin.name}</p>
+          </div>
+        </Link>
+      </td>
+      <td>{coin.symbol.toUpperCase()}</td>
+      <td>${coin.current_price.toLocaleString()}</td>
+      <td>
+        {coin.price_change_percentage_24h > 0 ? (
+          <p className="text-green-600">
+            {coin.price_change_percentage_24h.toFixed(2)}%
+          </p>
+        ) : (
+          <p className="text-red-600">
+            {coin.price_change_percentage_24h.toFixed(2)}%
+          </p>
+        )}
+      </td>
+      <td className="w-[180px] hidden md:table-cell">
+        ${coin.total_volume.toLocaleString()}
+      </td>
+      <td className="w-[180px] hidden md:table-cell">
+        ${coin.market_cap.toLocaleString()}
+      </td>
+      <td>
+        <Sparklines data={coin.sparkline_in_7d.price}>
+          <SparklinesLine color="teal" />
+        </Sparklines>
+      </td>
+    </tr>
+  );
+};
 
-
-            </td>
-            <td className='w-[180px] hidden md:table-cell'>${coin.total_volume.toLocaleString()}</td>
-            <td className='w-[180px] hidden md:table-cell'>${coin.market_cap.toLocaleString()}</td>
-            <td>
-                <Sparklines data={coin.sparkline_in_7d.price}>
-                    <SparklinesLine color='teal' />
-                </Sparklines>
-
-            </td>
-        </tr>
-
-
-
-    )
-}
-
-export default CoinItem
+export default CoinItem;
--- src/Components/SavedCoin.jsx
+++ src/Components/SavedCoin.jsx
@@ -1,56 +1,65 @@
-import React, { useState } from 'react';
-import { AiOutlineClose } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { UserAuth } from "../Context/AuthContext";
+import { collection, doc, onSnapshot } from "firebase/firestore";
+import { db } from "../Firebase/Firebase";
 
 const SavedCoin = () => {
-    const [coins, setCoins] = useState([])
-    return (
-        <div>
-            {coins.length === 0 ?
-                (<p>
-                    Currently you dont have any coins yes
-                    <Link to='/'>Click here to add coins</Link>
+  const [coins, setCoins] = useState([]);
+  const { user } = UserAuth();
 
-                </p>)
-                : (
-                    <table className='w-full border-collapse text-center'>
-                        <thead>
-                            <tr className='border-b'>
-                                <th className='px-4'>#</th>
-                                <th className='text-left'>Coin</th>
-                                <th className='text-left'>Remove</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {coins.map((coin) => {
-                                <tr className='h-[60px] overflow-hidden' key={coin.id}>
-                                    <td>{coin?.rank}</td>
-                                    <td>
-                                        <Link to={`/coin/${coin.id}`}>
-                                            <div className='flex items-center'>
-                                                <img className='w-8 mr-4' src={coin?.image} alt="/" />
-                                                <div>
-                                                    <p className='hidden sm:table-cell'>{coin?.name}</p>
-                                                    <p className='text-left text-sm text-gray-600'>{coin?.symbol.toUpperCase()}</p>
-                                                </div>
-                                            </div>
-                                        </Link>
-                                    </td>
-                                    <td className='pl-8'>
-                                        <AiOutlineClose className='cursor-pointer' />
-                                    </td>
-                                </tr>
+  useEffect(() => {
+    onSnapshot(doc(db, "users", `${user?.email}`), (doc) => {
+      setCoins(doc.data()?.bookmarks);
 
-                            })}
-                        </tbody>
-                    </table>
-                )}
+    });
+    // console.log("Getting data from firebase==>",bookmarks)
+  }, [user?.email]);
 
+  return (
+    <div>
+      {coins?.length === 0 ? (
+        <p>
+          Currently you dont have any coins yes
+          <Link to="/">Click here to add coins</Link>
+        </p>
+      ) : (
+        <table className="w-full border-collapse text-center">
+          <thead>
+            <tr className="border-b">
+              <th className="px-4">#</th>
+              <th className="text-left">Coin</th>
+              <th className="text-left">Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {coins?.map((coin) => {
+              <tr className="h-[60px] overflow-hidden" key={coin.id}>
+                <td>{coin?.rank}</td>
+                <td>
+                  <Link to={`/coin/${coin.id}`}>
+                    <div className="flex items-center">
+                      <img className="w-8 mr-4" src={coin?.image} alt="/" />
+                      <div>
+                        <p className="hidden sm:table-cell">{coin?.name}</p>
+                        <p className="text-left text-sm text-gray-600">
+                          {coin?.symbol.toUpperCase()}
+                        </p>
+                      </div>
+                    </div>
+                  </Link>
+                </td>
+                <td className="pl-8">
+                  <AiOutlineClose className="cursor-pointer" />
+                </td>
+              </tr>;
+            })}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
 
-
-        </div>
-    )
-}
-
-export default SavedCoin
+export default SavedCoin;
